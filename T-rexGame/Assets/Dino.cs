e4da75247acd5968e3f24c933dd7384b04b80ea6
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-01-07 13:33:06 +0100

    Add two scripts

---
+++ T-rexGame/Assets/Dino.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("ds");
+            rigidBody.AddForce(new Vector2(0, 375));
+        }
+    }
+}
---
+++ T-rexGame/Assets/RaceMoving.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceMoving : MonoBehaviour
+{
+    public float velocity;
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(velocity * Time.deltaTime, 0, 0);
+    }
+}
