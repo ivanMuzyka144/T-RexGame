c34e57f8c34235bd4e2111c5fa7fcf5b4d16d7d3
K-Krup <krupennyi@users.noreply.github.com>
2020-01-09 15:29:08 +0100

    Supply added

--- T-rexGame/Assets/LevelCreator.cs
+++ T-rexGame/Assets/LevelCreator.cs
@@ -11,6 +11,10 @@ public class LevelCreator : MonoBehaviour
 	GameObject[] cactuses;
 
 	[SerializeField]
+	GameObject[] supply;
+
+
+	[SerializeField]
 	GameObject ptero;
 
 	[SerializeField]
@@ -22,11 +26,18 @@ public class LevelCreator : MonoBehaviour
 	float frequency;
 
 	Vector3 currentPosition;
+	Vector3[] verticals;
 
 	private float nextSpawn;
 
 	void Start()
 	{
+		verticals = new Vector3[3];
+		verticals[0] = new Vector3(0, 0.5f);
+		verticals[1] = new Vector3(0, 1.5f);
+		verticals[2] = new Vector3(0, 2f);
+
+
 		currentPosition = new Vector3(-8, groundReference.position.y, groundReference.position.z);
 
 		for (int i =0; i<7; i++)
@@ -45,7 +56,7 @@ public class LevelCreator : MonoBehaviour
 	void CreateGround(Vector3 position)
 	{
 		
-		Instantiate(ground[Random.Range(0, ground.Length - 1)], position, groundReference.rotation);
+		Instantiate(ground[Random.Range(0, ground.Length)], position, groundReference.rotation);
 		currentPosition.x += groundStep;
 
 	}
@@ -53,14 +64,22 @@ public class LevelCreator : MonoBehaviour
 
 	void CreateObstacle(Vector3 position)
 	{
-		Instantiate(cactuses[Random.Range(0, cactuses.Length - 1)], position + new Vector3(0, 0.1f), Quaternion.identity);
+		Instantiate(cactuses[Random.Range(0, cactuses.Length)], position + new Vector3(0, 0.1f), Quaternion.identity);
+	}
+
+	void CreateSuply(Vector3 position)
+	{
+		Instantiate(supply[Random.Range(0, supply.Length)], position, Quaternion.identity);
 	}
 
 	int lastObst = 0;
+	int lastSup = 0;
+	int minHeight = 0;
 
 	void Update()
     {
-		
+		minHeight = 0;
+
 		frequency = groundStep /RunManager.GetInstance().GetSpeed();
 		if (Time.time >= nextSpawn)
 		{
@@ -70,11 +89,22 @@ public class LevelCreator : MonoBehaviour
 			{	
 				CreateObstacle(groundEdge.position);
 				lastObst = 0;
+				minHeight = 1;
 			}
 			else
 			{
 				lastObst++;
 			}
+			if(Random.Range(0, lastSup) >= 4)
+			{
+				Instantiate(supply[Random.Range(0, supply.Length)], groundEdge.position+verticals[Random.Range(minHeight,verticals.Length)], Quaternion.identity);
+				lastSup = 0;
+			}
+			else
+			{
+				lastSup++;
+			}
+
 		}
 		
 	}
