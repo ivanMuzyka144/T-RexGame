a75d95f38d2880ca4b488c110672993f4df973ac
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-01-09 13:01:37 +0100

    Create dino info script

---
+++ T-rexGame/Assets/Scripts/Tamagotchi/DinoInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoInfo : MonoBehaviour
+{
+    public int food = 100;
+    public int water = 100;
+    public int happiness = 100;
+
+    public DinoInfo Instance; 
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is cal
+   //led once per frame
+    void Update()
+    {
+        
+    }
+}
