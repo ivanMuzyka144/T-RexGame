5666393b6a84bd479b2ad4300e35d0c895782787
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-01-10 13:47:54 +0100

    Add score timer script

---
+++ T-rexGame/Assets/Scripts/ScoreTimer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTimer : MonoBehaviour
+{
+    float time = 0;
+    void Update()
+    {
+        Debug.Log(time);
+    }
+}
