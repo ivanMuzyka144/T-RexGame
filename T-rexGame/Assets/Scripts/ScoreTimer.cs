622e88684d7c1ec3caf03347a67ce838b3b247f2
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-01-10 14:55:51 +0100

    Create score timer algorithm

--- T-rexGame/Assets/Scripts/ScoreTimer.cs
+++ T-rexGame/Assets/Scripts/ScoreTimer.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreTimer : MonoBehaviour
 {
-    float time = 0;
+    double time = 0;
     void Update()
     {
-        Debug.Log(time);
+        time += float.Parse(Time.deltaTime.ToString("0.00"))*10;
+        char[] rowScore=Math.Truncate(time).ToString().ToCharArray();//100
+        Array.Reverse(rowScore);
+        string reversedString = new string(rowScore);
+        while (reversedString.Length != 5)
+        {
+            reversedString += "0";
+        }
+        char[] normalArray = reversedString.ToCharArray();
+        Array.Reverse(normalArray);
+        Debug.Log(new string(normalArray));
     }
 }
