d077f558b077087dfcde96dea6df6c44ce2932d7
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-01-10 09:48:32 +0100

    Add all windows

--- T-rexGame/Assets/Scripts/WindowSystem/WindowSystem.cs
+++ T-rexGame/Assets/Scripts/WindowSystem/WindowSystem.cs
@@ -10,10 +10,16 @@ public class WindowSystem : MonoBehaviour
         queueOfExecutors = new Queue<WindowElement>();
         WindowElement dinoElement = GameObject.Find("DinoWindowElement").GetComponent<WindowElement>();
         WindowElement tamagotchiElement = GameObject.Find("TamagotchiWindowElement").GetComponent<WindowElement>();
+        WindowElement clothesElement = GameObject.Find("ClothesWindowElement").GetComponent<WindowElement>();
+        WindowElement cablesElement = GameObject.Find("CablesWindowElement").GetComponent<WindowElement>();
         dinoElement.ChangeState(true);
         tamagotchiElement.ChangeState(false);
+        clothesElement.ChangeState(false);
+        cablesElement.ChangeState(false);
         queueOfExecutors.Enqueue(dinoElement);
         queueOfExecutors.Enqueue(tamagotchiElement);
+        queueOfExecutors.Enqueue(clothesElement);
+        queueOfExecutors.Enqueue(cablesElement);
     }
     void Update()
     {
